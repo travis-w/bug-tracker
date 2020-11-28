bd08d632e3f52d5cd58ed2cbdca39f0d0ecdb87a
Travis Weidenbenner <travis@thetravisw.com>
2020-11-27 22:56:30 -0600

    Add tabs for bug page

--- frontend/src/views/ViewBug.vue
+++ frontend/src/views/ViewBug.vue
@@ -10,17 +10,22 @@
 
     <div class="tab">
       <div class="tab__buttons">
-
+        <button @click="activeTab = 0">Info</button>
+        <button @click="activeTab = 1">Test History</button>
+        <button @click="activeTab = 2">Test Source</button>
       </div>
       <div class="tab__content">
         <div v-if="activeTab === 0">
+          Info Tab
+        </div>
+        <div v-if="activeTab === 1">
           <TestRun
             v-for="testResult in bug.testResults"
             :key="testResult._id"
             :testObj="testResult"
           />
         </div>
-        <div v-else-if="activeTab === 1">
+        <div v-else-if="activeTab === 2">
           <Editor class="h-52" v-model="bug.test" :read-only="true" />
         </div>
       </div>
@@ -62,3 +67,17 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.tab__buttons {
+  @apply mb-2;
+  
+  button {
+    @apply p-2;
+  }
+
+  button + button {
+    @apply border-l;  
+  }
+}
+</style>

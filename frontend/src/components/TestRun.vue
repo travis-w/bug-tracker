63e96af55a08bba214d8082046ebe9b7d817683a
Travis Weidenbenner <travis@thetravisw.com>
2020-11-27 21:07:50 -0600

    Start display of test history on bug pages

---
+++ frontend/src/components/TestRun.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="test_run" :class="{ 'test_run--pass': testObj.passed }">
+    <div class="">
+      {{ testHeader }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { toRefs, computed } from "vue";
+import { DateTime } from "luxon";
+
+export default {
+  props: {
+    testObj: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const { testObj } = toRefs(props);
+
+    const passedTests = testObj.value.tests.filter(x => { console.log(x); return x.state === "passed"}).length;
+
+    const formatDate = (date) => {
+      return DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_FULL);
+    };
+
+    const testHeader = computed(() => {
+      return `${formatDate(testObj.value.date)} (${passedTests} / ${testObj.value.tests.length})`;
+    });
+
+    return {
+      passedTests,
+      testHeader
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+.test_run {
+  @apply p-3 border relative;
+
+  &::after {
+    @apply bg-red-500 absolute inset-y-0;
+    content: ' ';
+    content: " ";
+    width: 8px;
+    left: -1px;
+  }
+
+  &.test_run--pass::after {
+    @apply bg-green-500;
+  }
+
+  & + & {
+   @apply border-t-0; 
+  }
+}
+</style>
--- frontend/src/views/ViewBug.vue
+++ frontend/src/views/ViewBug.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>
-    <h1>{{ bug.name }}</h1>
-    <button>Delete</button>
-    <Editor class="h-52" v-model="bug.test" :read-only="true" />
-    <video :src="`http://localhost:8081/videos/${bug._id}/${bug.testResults?.[0]._id}/run.mp4`" controls></video>
+  <div v-if="bug">
+    <h1 class="page-title">{{ bug.name }}</h1>
+    <video
+      v-if="video"
+      class="m-auto max-w-4xl"
+      :src="video"
+      controls
+    />
+
+    <div class="tab">
+      <div class="tab__buttons">
+
+      </div>
+      <div class="tab__content">
+        <div v-if="activeTab === 0">
+          <TestRun
+            v-for="testResult in bug.testResults"
+            :key="testResult._id"
+            :testObj="testResult"
+          />
+        </div>
+        <div v-else-if="activeTab === 1">
+          <Editor class="h-52" v-model="bug.test" :read-only="true" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,24 +34,30 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 import Editor from "@/components/Editor";
+import TestRun from "@/components/TestRun";
 
 const BASE_URL = process.env.VUE_APP_API_BASE;
 
 export default {
-  components: { Editor },
+  components: { Editor, TestRun },
   setup() {
     const route = useRoute();
-    const bug = ref({});
+    const bug = ref(null);
+    const video = ref(null);
+    const activeTab = ref(0);
 
     // TODO: Look into asyc setup/vue suspense
     ky.get(`${BASE_URL}/bugs/${route.params.bugId}`)
       .then(data => data.json())
       .then(res => {
         bug.value = { ...res };
+        video.value = `http://localhost:8081/videos/${bug.value._id}/${bug.value.testResults?.[0]._id}/run.mp4`;
       });
 
     return {
-      bug
+      bug,
+      video,
+      activeTab
     };
   }
 };
